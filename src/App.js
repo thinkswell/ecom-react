fbabd81569a9c635edabefef2941f438c7118b3b
thinkswell <krishna23712@gmail.com>
2020-10-31 00:41:19 +0530

    added test route to card , used withRouter Higher Order Component, HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.scss";
 import Homepage from "./pages/Homepage/Homepage";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => {
+  return <h1>Hats Page</h1>;
+};
 
 export default function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Card.scss";
+import { withRouter } from "react-router-dom";
 
-export default function Card({ title, imageUrl, size }) {
+function Card({ title, imageUrl, size, linkUrl, history, match }) {
   return (
-    <div className={` ${size} card`}>
+    <div
+      className={` ${size} card`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <img src={imageUrl} alt={title} className="background-image" />
       <div className="card__about">
         <h1 className="card__about--title">{title}</h1>
@@ -12,3 +16,5 @@ export default function Card({ title, imageUrl, size }) {
     </div>
   );
 }
+
+export default withRouter(Card);
